e201c9d96726062ccc98e736c07a5db57ee95fb0
gitmackfree <freemanmackenzie06@gmail.com>
2024-11-06 17:14:50 -0500

    Created views.

--- mysite/handbook/urls.py
+++ mysite/handbook/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'handbook'
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('policy_sections/', views.policy_sections, name='policy_sections'),
+    path('section/<str:section_number>/', views.section, name='section'),
 ]
--- mysite/handbook/views.py
+++ mysite/handbook/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
+from .models import PolicySection, Policy
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the handbook index.")
+    return render(request, 'handbook/index.html')
+
+# Display all sections with their policies listed underneath.
+def policy_sections(request):
+    sections = PolicySection.objects.prefetch_related('policies')
+    return render(request, 'handbook/policy_sections.html', {'sections': sections})
+
+# Display all details for a specific section.
+def section(request, section_number):
+    section = get_object_or_404(PolicySection, number=section_number)
+    policies = Policy.objects.filter(section=section).order_by('number')
+    return render(request, 'handbook/section.html', {'section': section, 'policies': policies})
